366f1c959ff9239bf922314718baa7e69dea20d8
DevanshPatel10 <159623633+DevanshPatel10@users.noreply.github.com>
2025-02-28 17:17:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,3 @@
-# import streamlit as st
-
-# st.title("🎈 My new app")
-# st.write(
-#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-#)
-
-
-
-
-#  %%writefile placementapp.py
-
 import streamlit as st
 import pandas as pd
 import numpy as np 
